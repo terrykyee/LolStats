d29b49017a4a67949aee113391095c2ca8fb5f52
Terry Yee <tyee@pason.com>
2019-03-07 22:48:08 -0700

    - fix cors issue

--- server/src/summonersController.js
+++ server/src/summonersController.js
@@ -14,6 +14,8 @@ router.use(bodyParser.urlencoded({extended: true}));
 router.use(bodyParser.json());
 router.use(cors());
 router.use((request, response, next) => {
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   response.header('Content-Type', 'application/json');
   next();
 });
