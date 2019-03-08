51b6f4473eef00dd3117748ac9fe345f0f8ac2ca
Terry Yee <tyee@pason.com>
2019-03-08 00:41:19 -0700

    - fix cors issue

--- server/src/app.js
+++ server/src/app.js
@@ -8,13 +8,6 @@ import cors from 'cors';
 // express application
 const app = express();
 app.use(cors());
-/*app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", '*');
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-  next();
-});*/
 app.use('/api', summonersController);
 
 export default app;
